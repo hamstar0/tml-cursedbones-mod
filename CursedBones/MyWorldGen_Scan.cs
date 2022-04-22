a2b03b68ab3beafa25e0318ce41d29473a8d6bc3
hamstar0 <webutek@gmail.com>
2022-04-21 20:37:33 -0700

    v1.1.1.3 - Another attempt at stopping open air patches (works?)

--- CursedBones/MyWorldGen_Scan.cs
+++ CursedBones/MyWorldGen_Scan.cs
@@ -1,24 +1,28 @@
 using System;
 using Terraria;
 using Terraria.World.Generation;
+using ModLibsCore.Libraries.Debug;
 
 
 namespace CursedBones {
 	partial class CursedBonesPatchesGen : GenPass {
-		private static bool HasOpenAirNearby( int tileX, int tileY ) {
-			int rad = 2;
+		private static bool HasOpenAirNearby( int tileX, int tileY, int rad=2 ) {
 			int minX = tileX - rad <= 0 ? 1 : tileX - rad;
 			int maxX = tileX + rad >= Main.maxTilesX ? Main.maxTilesX - 1 : tileX + rad;
 			int minY = tileY - rad <= 0 ? 1 : tileY - rad;
 			int maxY = tileY + rad >= Main.maxTilesY ? Main.maxTilesY - 1 : tileY + rad;
 
-			for( int x = minX; x<maxX; x++ ) {
-				for( int y = minY; y<maxY; y++ ) {
+			for( int x = minX; x < maxX; x++ ) {
+				for( int y = minY; y < maxY; y++ ) {
 					Tile tile = Main.tile[x, y];
-
-					if( tile?.active() != true && tile?.wall == 0 ) {
-						return true;
+					if( tile?.active() == true ) {
+						continue;
 					}
+					if( tile != null && tile.wall != 0 ) {
+						continue;
+					}
+
+					return true;
 				}
 			}
 
@@ -39,13 +43,6 @@ namespace CursedBones {
 
 			hasMatter = tile?.active() == true;
 
-			// No 'open air' tiles
-			if( y < WorldGen.worldSurface ) {	//Main.worldSurface?
-				if( CursedBonesPatchesGen.HasOpenAirNearby(x, y) ) {
-					return false;
-				}
-			}
-
 			if( hasMatter ) {
 				return false;
 				/*if( CursedBonesPatchesGen.IsEarthType(tile.type) ) {
@@ -57,6 +54,13 @@ namespace CursedBones {
 					// Exclude all else
 					return false;
 				}*/
+			}
+
+			// No 'open air' tiles on surface
+			if( y < WorldGen.worldSurface ) {	//Main.worldSurface?
+				if( CursedBonesPatchesGen.HasOpenAirNearby(x, y) ) {
+					return false;
+				}
 			}
 
 			return CursedBonesPatchesGen.HasCardinallyAdjacentValidAttachableTileForBone( x, y );
